040ecc4b5964aae209e77014af368c8fae89cfed
Felilepe <felipesepulveda536@gmail.com>
2026-06-29 11:29:05 -0300

    feat: implementa header to TAD grafo

---
+++ src/graph.h
@@ -0,0 +1,23 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <stdbool.h>
+
+typedef void* Graph;
+typedef void* Node;
+
+Graph graphCreate(int n, int flag);
+
+bool graphAddNode(Graph g, Node n);
+
+bool graphRemoveNode(Graph g, Node n);
+
+Node graphGetNode(Graph g, int n);
+
+int graphGetSize(Graph g);
+
+bool graphNodeExists(Graph g, Node 
+
+void graphForEach(Graph h, void (*aux)(void* item, void* aux_data), void* aux_data);
+
+#endif
